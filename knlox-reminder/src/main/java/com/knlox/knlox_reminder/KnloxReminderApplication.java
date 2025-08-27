6be4fdafe7d3cabc4fce47fdc2090a9a909ba269
knlox <thirdfrank11@gmail.com>
2025-08-27 10:21:01 +0800

    initial commit

---
+++ knlox-reminder/src/main/java/com/knlox/knlox_reminder/KnloxReminderApplication.java
@@ -0,0 +1,13 @@
+package com.knlox.knlox_reminder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KnloxReminderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KnloxReminderApplication.class, args);
+	}
+
+}
---
+++ knlox-reminder/src/test/java/com/knlox/knlox_reminder/KnloxReminderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.knlox.knlox_reminder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KnloxReminderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
